af7678202b24672b7dadfabfbbf1db99476cc282
martynas <martyskole@gmailc.om>
2019-10-22 18:43:16 +0200

    Fikk test 1 & 2 til å passere. Overlater Test3 til deg

--- webshop/src/main/java/no/kristiania/jdbc/ProductDao.java
+++ webshop/src/main/java/no/kristiania/jdbc/ProductDao.java
@@ -1,12 +1,17 @@
 package no.kristiania.jdbc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDao {
+
+    private List<String> products = new ArrayList<>();
+
     public void insertProduct(String apples) {
+        products.add(productName);
     }
 
     public List<String> listAll() {
-        return null;
+        return products;
     }
 }
--- webshop/src/test/java/no/kristiania/jdbc/WebshopTest.java
+++ webshop/src/test/java/no/kristiania/jdbc/WebshopTest.java
@@ -2,14 +2,21 @@ package no.kristiania.jdbc;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WebshopTest {
 
     @Test
-    void shouldRetrieveStoredProducts() {
+    public void shouldRetrieveStoredProducts() {
         ProductDao dao = new ProductDao();
-        dao.insertProduct("Apples");
-        assertThat(dao.listAll()).contains("Apples");
+        String productName = pickOne(new String[] {"Apples", "Bananas", "Coconuts", "Dates"});
+        dao.insertProduct(productName);
+        assertThat(dao.listAll()).contains(productName);
+    }
+
+    private String pickOne(String[] strings) {
+        return strings[new Random().nextInt(strings.length)];
     }
 }
